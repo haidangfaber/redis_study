91fe042808bca60b499cec755ef406af87f68289
dangFaber <nguyenlehaidang@fabercompany.co.jp>
2024-11-11 16:18:52 +0700

    add config

---
+++ src/main/java/com/example/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+
+@Configuration
+public class RedisConfig {
+    @Value("${redis.host}")
+    private String redisHost;
+
+    @Value("${redis.port}")
+    private int redisPort;
+
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        // Tạo Standalone Connection tới Redis
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort));
+    }
+
+
+    @Bean
+    @Primary
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        // Tạo một RedisTemplate
+        // Với Key là Object
+        // Value là Object
+        // RedisTemplate giúp chúng ta thao tác với Redis
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
